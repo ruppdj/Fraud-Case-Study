46df838089b585b9551112089eecd59d014b27b4
Dan Rupp <ruppdj2003@yahoo.com>
2017-06-30 13:18:47 -0500

    Update app.py and html

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def api_works():
 def api_broken():
     return 'Nothing is'
 
+#testing
 @app.route('/hello')
 def api_hello():
     if 'name' in request.args:
@@ -25,8 +26,34 @@ def api_hello():
     else:
         return 'Hello World!'
 
+
 @app.route('/score', methods = ['GET','POST'])
+'''
+the following codes will set up score page.
+the score page will display
+1. overall prediction results which contains
+    a: total number of non-flaud events
+    b: total number of possibly flaud events
+    c: total number of highly flaud events
+
+2. tables of non-flaud, possibly flaud and highly flaud events.
+    each table contains
+    a: Object_ID
+    b: Organization Name
+    c: Country of the event
+    d: Prediction percentage of event being non-flaud event
+    e: Prediction percentage of event being possibly flaud event
+    f: Prediction percentage of event being highly Fraud event
+    g: timestamp of the event
+    h: radio button which asks users if users contacted the event Organization
+        - if user select "yes" and press submit button, program will delete
+        event and event information from database
+        - if user select "no" and press submit button, event information
+        including event id, organization name and their contact will be displayed
+'''
 def score():
+    # access mongodb and store all non-flaud, possibly flaud and highly flaud
+    # events into 3 different lists
     mongo_client = MongoClient()
     db = mongo_client.p1_database
     coll = db.web_input
@@ -44,19 +71,42 @@ def score():
         fr.append(i)
     #return "this is not_fraud: {}".format(not_fraud)
     mongo_client.close()
+
+    # returns lists and length of lists to score.html template.
+    # information will be used to create tables
     return render_template('score.html',  nf= nf,len_nf=len(nf),\
     mb = mb, len_mb=len(mb), fr = fr, len_fr=len(fr))
 
 @app.route('/feedback', methods = ['GET','POST'])
+'''
+the following codes will set up feedback page.
+the feedback page will be displayed only when user select radio button and
+click submit button in the score page.
+
+the feedback page contains following:
+1. If user selected "yes" radio button:
+    - open mongodb and delete event with specific object id from the database.
+    - display message "object id is removed from the database!"
+2. If user selected "no" radio button:
+    - open feedback page with table containing event id, event organization
+    name and their e-mail address.
+    - Since we do not have actual e-mail address, the page displays fake
+    e-mail address (ilovemoney@gmail.com)
+'''
 def feedback():
+    # gets information from score page
     switch = int(request.form.get('switch'))
     orgname = str(request.form.get('orgname'))
     inputdata = int(request.form.get('id'))
     if switch == 1:
+        # if user selected "Yes", opens up mongodb and delete that
+        #specific event from the database
         mongo_client = MongoClient()
         db = mongo_client.p1_database
         coll = db.web_input
         coll.remove({"object_id":(inputdata)})
+        # the no_change will become a switch to initiate if statement in
+        # our feedback page
         no_change = 1
     else:
         no_change = 0
